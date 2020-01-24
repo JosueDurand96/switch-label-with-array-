fc8e59cc811b87812eec4413de658548b716becf
Josue <josue.durand8@gmail.com>
2020-01-23 19:19:29 -0500

    Initial Commit

---
+++ Ejemplo - Sesion3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejemplo - Sesion3
+//
+//  Created by Josue Durand on 1/23/20.
+//  Copyright © 2020 Josue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
