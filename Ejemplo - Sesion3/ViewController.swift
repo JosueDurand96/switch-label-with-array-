61dd0f250755390206997fcf47dfde07676b8b23
Josue <josue.durand8@gmail.com>
2020-01-23 20:00:59 -0500

    first

--- Ejemplo - Sesion3/ViewController.swift
+++ Ejemplo - Sesion3/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var misegment: UISegmentedControl!
+    @IBOutlet weak var contenedorImg: UIImageView!
+    @IBOutlet weak var txtNombreCiudad: UILabel!
+    @IBOutlet weak var miSwitch: UISwitch!
+    
+    @IBOutlet weak var txtdescripcion: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func procesando(_ sender: Any) {
+        let selectedIndex = misegment!.selectedSegmentIndex
+        print("index seleccionado",selectedIndex)
+        let selectedName = misegment.titleForSegment(at: selectedIndex)
+        print("descripcion",selectedName)
+    }
+    
 }
 
